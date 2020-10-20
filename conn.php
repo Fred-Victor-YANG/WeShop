78ea347dfe128660ea54da8e7cfbd498f9845cd0
Fred-Victor-YANG <frederic.victor.yang@gmail.com>
2020-10-20 23:35:37 +0200

    init branch backend

--- conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once 'config.php';
-    $conn=mysqli_connect($servername, $username, $password, $dbname);
-    if (mysqli_connect_errno())
-    {
-        echo "error: " . mysqli_connect_error();
-    }
-?>
